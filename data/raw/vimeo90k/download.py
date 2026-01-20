45249e9bd2b4e1a8611944d59ba529e9f1b3aa6c
Ok2dot0 <wolborskijakub@gmail.com>
2026-01-20 14:32:58 +0100

    Enhance download script to manage file paths and cleanup extracted directories

--- data/raw/vimeo90k/download.py
+++ data/raw/vimeo90k/download.py
@@ -2,6 +2,7 @@ import requests
 import zipfile
 import os
 from tqdm import tqdm
+import shutil
 
 url = 'http://data.csail.mit.edu/tofu/dataset/vimeo_septuplet.zip'
 filename = 'vimeo90k.zip'
@@ -21,4 +22,23 @@ with open(filename, 'ab') as out_file:
                 pbar.update(len(chunk))
 
 with zipfile.ZipFile(filename, 'r') as zip_ref:
-    zip_ref.extractall('vimeo90k')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    dest_zip = os.path.join(script_dir, os.path.basename(filename))
+    if os.path.abspath(filename) != os.path.abspath(dest_zip):
+        try:
+            shutil.move(filename, dest_zip)
+            filename = dest_zip
+        except Exception:
+            pass
+
+    src_dir = os.path.abspath('vimeo90k')
+    dest_dir = os.path.join(script_dir, 'vimeo90k')
+    if os.path.abspath(src_dir) != os.path.abspath(dest_dir) and os.path.exists(src_dir):
+        try:
+            if os.path.exists(dest_dir):
+                shutil.rmtree(src_dir)
+            else:
+                shutil.move(src_dir, dest_dir)
+        except Exception:
+            pass
